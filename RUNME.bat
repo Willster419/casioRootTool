392caaa10c80d4560ac01a6f007c326402532af1
willster419 <willster419@outlook.com>
2015-05-03 00:20:58 -0400

    V2.1 - incorporated the "save locked device" into the main RUNME program. Fixed a few minor bugs

--- RUNME.bat
+++ RUNME.bat
@@ -20,8 +20,9 @@ ECHO     *Requires root
 ECHO 7 - View Phone Version
 ECHO 8 - Fix Wifi Error
 ECHO     *Requries root
+ECHO 9 - Save your locked device
 ECHO.
-ECHO 9 - Exit this tool
+ECHO 10 - Exit this tool
 ECHO.
 ECHO #To stop the script at any time, press Ctrl+C
 ECHO.
@@ -35,7 +36,8 @@ IF %M%==5 GOTO DBR
 IF %M%==6 GOTO IB
 IF %M%==7 GOTO VPV
 IF %M%==8 GOTO FWE
-IF %M%==9 GOTO EXIT
+IF %M%==9 GOTO SLD
+IF %M%==10 GOTO EXIT
 
 rem the root options menu
 :RU
@@ -1213,6 +1215,204 @@ adb shell su -c "system/xbin/busybox cp /data/local/tmp/init.qcom.sdio.sh /syste
 adb shell su -c "rm /data/local/tmp/init.qcom.sdio.sh"
 adb shell su -c "chmod 777 /system/etc/init.qcom.sdio.sh"
 ECHO #Done!
+pause
+GOTO MENU
+
+:SLD
+CLS
+ECHO #Casio Locked Device Recovery Add-on by: Willster419
+ECHO #By using the script you understand that this is done at YOUR own risk.
+ECHO #Make sure drivers are installed and adb is enabled!
+ECHO #Phone screen MUST be on!
+ECHO #Due to the different firmware versions of devices I cannot verify
+ECHO #that this works 100 percent.
+ECHO #By continuing you understand that this will WIPE YOUR PHONE
+pause
+ECHO #Detecting Device...
+ECHO #If this hangs for more than a minute, then you don't have adb on.
+adb wait-for-device
+ECHO #Device detected, waiting for the screen to be on...
+:checkLoopScreenOn
+adb shell sleep 1
+for /f %%i in ('adb shell "dumpsys input_method | grep mScreenOn | cut -c 31-35"') do set var2=%%i
+if "false"=="%var2%" (
+goto checkLoopScreenOn
+)
+ECHO #Screen on, attempting recovery! Approx time is 20 seconds
+ECHO #Do NOT touch the screen at this time!
+adb shell input keyevent 5
+adb shell sleep 2
+adb shell sendevent /dev/input/event1 3 53 340
+adb shell sendevent /dev/input/event1 3 54 650
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+
+adb shell sendevent /dev/input/event1 3 53 0
+adb shell sendevent /dev/input/event1 3 54 0
+adb shell sendevent /dev/input/event1 3 50 0
+adb shell sendevent /dev/input/event1 3 48 0
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+
+adb shell sendevent /dev/input/event1 3 53 340
+adb shell sendevent /dev/input/event1 3 54 625
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+
+adb shell sendevent /dev/input/event1 3 53 0
+adb shell sendevent /dev/input/event1 3 54 0
+adb shell sendevent /dev/input/event1 3 50 0
+adb shell sendevent /dev/input/event1 3 48 0
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+
+adb shell input keyevent 14
+adb shell input keyevent 14
+adb shell input keyevent 13
+adb shell input keyevent 11
+adb shell input keyevent 14
+adb shell input keyevent 9
+adb shell input keyevent 13
+adb shell input keyevent 5
+adb shell input keyevent 7
+adb shell input keyevent 7
+adb shell input keyevent 7
+adb shell input keyevent 7
+adb shell input keyevent 7
+adb shell input keyevent 7
+
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 650
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 625
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 600
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 575
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 550
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 525
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 525
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 500
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 475
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 450
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 0
+adb shell sendevent /dev/input/event1 3 54 0
+adb shell sendevent /dev/input/event1 3 50 0
+adb shell sendevent /dev/input/event1 3 48 0
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sleep 1
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 650
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+
+adb shell sendevent /dev/input/event1 3 53 0
+adb shell sendevent /dev/input/event1 3 54 0
+adb shell sendevent /dev/input/event1 3 50 0
+adb shell sendevent /dev/input/event1 3 48 0
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 250
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+
+adb shell sendevent /dev/input/event1 3 53 0
+adb shell sendevent /dev/input/event1 3 54 0
+adb shell sendevent /dev/input/event1 3 50 0
+adb shell sendevent /dev/input/event1 3 48 0
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 750
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+
+adb shell sendevent /dev/input/event1 3 53 0
+adb shell sendevent /dev/input/event1 3 54 0
+adb shell sendevent /dev/input/event1 3 50 0
+adb shell sendevent /dev/input/event1 3 48 0
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb shell sendevent /dev/input/event1 3 53 240
+adb shell sendevent /dev/input/event1 3 54 500
+adb shell sendevent /dev/input/event1 3 50 5
+adb shell sendevent /dev/input/event1 3 48 122
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+
+adb shell sendevent /dev/input/event1 3 53 0
+adb shell sendevent /dev/input/event1 3 54 0
+adb shell sendevent /dev/input/event1 3 50 0
+adb shell sendevent /dev/input/event1 3 48 0
+adb shell sendevent /dev/input/event1 0 2 0
+adb shell sendevent /dev/input/event1 0 0 0
+adb reboot bootloader
+ECHO #waiting for fastboot...
+ping 1.1.1.1 -n 1 -w 20000 > nul
+ECHO #wiping phone...
+fastboot -i 0x0409 erase userdata
+fastboot -i 0x0409 erase cache
+fastboot -i 0x0409 reboot
+ECHO #Recovery Complete! You're device is wiped and you have
+ECHO #access to it again! Enjoy!
 pause
 GOTO MENU
 
